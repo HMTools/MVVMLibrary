6fbee6e3e3ed6ef54b243912ca701769c474c632
HMTools <m4t4ngm@gmail.com>
2021-04-04 16:17:22 +0300

    Added String Wrapper

---
+++ Wrappers/StringWrapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MVVMLibrary.Wrappers
+{
+    public class StringWrapper
+    {
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region Properties
+        private string val;
+
+	    public string Value
+	    {
+		    get { return val;}
+		    set { val = value; NotifyPropertyChanged();}
+	    }
+
+        #endregion
+    }
+}
